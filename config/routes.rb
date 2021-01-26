d6c338060fdba9149b2cc54b24b873f217dd2cea
Noura <contact.nouraloudani@gmail.com>
2021-01-26 13:30:25 -0500

    p

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,7 @@ Rails.application.routes.draw do
   # resources :cocktails
   resources :users
 
-  get'/cocktails', to: 'cocktails#index', as: 'cocktails'
-
+  get '/cocktails', to: 'cocktails#index', as: 'cocktails'
   get '/categories', to: 'categories#index', as: 'categories'
-
-
- 
-
-# //new reviews
-# //create review
-# //reviews index
-# //edit review
-# //update review
-
-# //new user
-# //create user
-# //destroy user
-
-# //cocktails index
-
-# //categories index
+  
+end
