96cb4ee2bbb4769540e94a51dfb84d73f62ca537
clairedeboer <claire.deboer@gmail.com>
2021-01-26 19:06:21 -0500

    reviews routes and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def create
+    review = Review.create(review_params)
+    render json: review
+  end
+
+  def update
+     review = Review.find(params[:id])
+     review.update(review_params)
+     render json: review
+  end
+
+  private
+  def review_params
+    #might need require
+    params.permit(:user_id, :cocktail_id, :rating, :review_text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get '/cocktails', to: 'cocktails#index', as: 'cocktails'
   get '/categories', to: 'categories#index', as: 'categories'
   
+  post '/reviews', to: 'reviews#create'
+  patch'/reviews/:id', to: 'reviews#update'
+
 end
