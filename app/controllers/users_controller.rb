98bf6e33da81569ae320180adf87a856850db717
Noura <contact.nouraloudani@gmail.com>
2021-01-27 10:56:05 -0500

    added users controller functions and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+    def index 
+        users = User.all
+        render json: users
+    end 
+    
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end 
+
+
+
+    def create
+        user = User.create(user_params)
+
+        render json: user
+    end
+
+    def delete
+        user = User.find(params[:id])
+        user.destroy!
+        render json: {}
+    end
+    
+
+  private 
+
+    def user_params
+        params.permit(:username, :age)
+    end 
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   # resources :reviews
   # resources :categories
   # resources :cocktails
-  resources :users
+  # resources :users
+  
+  # user routes
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  post "/users", to: "users#create"
+  delete "/delete", to: "users#delete"
+
+  #categories routes
+  get '/categories', to: 'categories#index', as: 'categories'
+  
 
   get '/cocktails', to: 'cocktails#index', as: 'cocktails'
-  
-  get '/categories', to: 'categories#index', as: 'categories'
   
   post '/reviews', to: 'reviews#create'
   patch'/reviews/:id', to: 'reviews#update'
