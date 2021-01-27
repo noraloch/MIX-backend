495ba8058cf0863d8da95664bf362f9d27c7f098
Noura <contact.nouraloudani@gmail.com>
2021-01-27 12:25:32 -0500

    corrected validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :cocktails, through: :reviews
 
-  validates :email, uniqueness: true, uniqueness: {case_sensitive: false}
-
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
 end
